33645fbbf95298c81e29d154272dc2f27bc712b9
Lamine Aknouche <42774809+aknouche@users.noreply.github.com>
2019-10-21 15:51:44 +0200

    Add files via upload

--- javascript/index.js
+++ javascript/index.js
@@ -22,3 +22,19 @@ function hideMoreInfo(buttonID) {
 function alertMe() {
 	alert(new Date().toLocaleDateString());
 }
+
+//Method that gets a random text from an array and sets it at an html paragraph
+//function generateCitation() {
+	let citations = ['Kunskapen börjar med att tiga, sedan med att lyssna, sedan med att  memorera, sedan med att handla och sedan med att sprida.',
+	'Om en person söker kunskap för att handla utmed den, gläds han av sin kunskap. Om han gör det för andra syften, blir han bara alltmer arrogant.',
+	'Det finns inget gott i ett liv om det inte levs av en som tiger och begriper eller talar och är lärd.',
+	'Tystnaden skyddar tungan från förvrängt tal, falska ord och överflödigt prat. Därtill ger den respekt till den tyste.',
+	'Människorna med det bästa förståndet är de som inte blir ilskna. Människorna med det bäst förberedda svaret är de som inte blir arga.',
+	'Ilskan sår ånger. En person har lättare att korrigera det förstörda innan han blir arg än efter att han har blivit arg.',
+	'Oförmågan gifte sig med förseningen och fick ångern som barn.',
+	'Givmildhet är kärlek och berömvärt och snålhet är hat och klandervärt. Det finns inget gott i pengar utan givmildhet.',
+	]
+
+	let randomCitation =  citations[Math.floor(Math.random() * citations.length)];
+	document.getElementById("citationParagraph").innerHTML = randomCitation;
+//}
